decd86062b4b884c3963d909e5c621dc312e0d35
Dave Stevens <dave@theinternationalgame.com>
2010-11-21 22:05:09 -0800

    gitting this committed

---
+++ app.js
@@ -0,0 +1,120 @@
+/*
+/#!/articles/
+/#!/articles/article-title-url-sanitized
+/#!/articles/article-title-url-sanitized/tags-appended-here (limit of 1st 4 tags)
+/#!/twitter/
+/#!/twitter/status_id
+/#!/elsewhere/
+/#!/elsewhere/facebook
+/#!/work/
+/#!/work/company-name-project-name
+/#!/work/company-name-project-name/skill-tags-appended-here (limit of 1st 4 skill tags)
+
+
+so, we parse out everything after #! and send it via ajax to get the next page.
+part 1: section
+part 2: specific item
+part 3: additional info
+*/
+
+var util = require('sys'),
+    http = require('http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs'),
+    m = require('mustache'), // switch to mu by rayMorgan
+    c = require('./content'),
+    t = require('./templates'),
+    a = require('./articles');
+
+function defaultView(content) {
+  this.content = content;
+};
+
+defaultView.prototype = {
+  title: 'Dave Stevens (shakefon) is not your #1 enemy',
+  meta: '<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">',
+  styles: '<link rel="stylesheet" type="text/css" href="/styles/style.css"/>\n<!--[if !IE]>-->\n<link type="text/css" rel="stylesheet" media="only screen and (max-device-width: 480px)" href="/styles/mbl.css">\n<!--<![endif]-->'
+};
+
+http.createServer(function(request, response){  
+  var p = url.parse(request.url), 
+      base = p.pathname, 
+      filename = path.join(process.cwd(),base),
+      h = (base.indexOf('.css') != -1) ? 'text/css' : (base.indexOf('.js') != -1) ? 'text/javascript' : (base.indexOf('.png') != -1) ? 'image/png' : (base.indexOf('.jpg') != -1) ? 'image/jpeg' : (base.indexOf('.gif') != -1) ? 'image/gif' : 'text/html';
+
+  if (base.length === 0 || base == '/') {
+    
+    // HOMEPAGE
+    
+    var view = new defaultView(c.home);
+    html = m.to_html(t.page,view);
+
+    response.writeHead(200,'text/html');
+    response.write(html);
+    response.end();
+    
+  } else if (base == '/aea') {
+
+    // THIS WILL BE EXPANDED OUT TO CONTROL WHATEVER WE'RE SHOWING - SECTIONS ABOVE ETC.
+    // IN FACT, THIS SECTION CAN BE A VANITY URL HANDLER. HAVE AN ARRAY OF VANITY URLS AND THEIR
+    // TEMPLATES TO USE, SO WE CAN DO AEA, CES, FOWD, WHATEVER.    
+    var view = new defaultView(c.aea);
+    view.title = 'I\'m Dave Stevens: We Met At An Event Apart San Diego 2010';
+    view.meta = '<meta name="robots" content="noindex,nofollow,noarchive"/><meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">';
+    var html = m.to_html(t.page,view);
+
+    response.writeHead(200,'text/html');
+    response.write(html);
+    response.end();
+
+  } else if (base.split('/')[1] == 'articles') {
+
+    var keys = {
+      'setting-up-node-js-and-couchdb-on-webfaction':'01'
+    },    
+        keysTitles = {
+      '01':'Setting up node.js and CouchDB on Webfaction'
+    },
+        keysContent = {
+      '01': a.article01    
+    };
+    
+    var persistent = ['<div id="photo"><img src="/imgs/dave.jpg"/><span>I look like this</span></div>','</div>','<div id="twitter"><a href="http://twitter.com/shakefon">YOU SHOULD FOLLOW ME ON TWITTER.</a></div>'].join(''),
+        urlKey = base.split('/')[2],
+        key = keys[urlKey],
+        view = new defaultView(keysContent[key] + persistent);
+    
+    view.title = keysTitles[key] || 'Article';
+    var html = m.to_html(t.page,view);
+    response.writeHead(200,'text/html');
+    response.write(html);
+    response.end();
+    
+  } else {
+
+    // STATIC FILES TO BE SERVED
+    path.exists(filename, function(exists) {  
+      if(!exists) {  
+        response.writeHead(404, {'Content-Type':h});
+        response.write("404 Not Found\n");  
+        response.end();
+        return;  
+      }  
+
+      fs.readFile(filename, "binary", function(err, file) {  
+        if(err) {  
+          response.writeHead(500, {'Content-Type':'text/plain'});
+          response.write(err + "\n");  
+          response.end();
+          return;  
+        }             
+        response.writeHead(200, {'Content-Type':h});          
+        response.write(file, "binary");  
+        response.end();
+      });  
+    });
+    
+  }   
+}).listen(56246,"127.0.0.1");
+console.log("server listening on port 56246");
---
+++ content.js
@@ -0,0 +1,53 @@
+// This'll be CouchDB'd in full whack. Temp quick win here.
+
+this.home = [
+  '<div id="content">',
+  '<h1>Dave Stevens <span>Enthusiast.</span></h1>',
+  '<p class="first">',
+    'Dave Stevens is a developer who loves JavaScript and all things Mobile. <em>I</em> am Dave Stevens. Currently, I\'m exploring the possibilities of <a href="http://nodejs.org">node.js</a> and getting',
+    'very, very excited about what can be achieved. I\'d love to talk to you.',
+  '</p>',
+  '<h2>Articles</h2>',
+  '<ul>',
+    '<li class="dev"><a href="/articles/setting-up-node-js-and-couchdb-on-webfaction">Setting up node.js and CouchDB on Webfaction</a> A quick guide to getting set up with node &amp; CouchDB on Webfaction hosting using git and the control panel.</li>',
+  '</ul>',
+  '<h2>On the subject of writing</h2>',
+  '<p>',
+    'Like most developers I\'ve ever met, I tend to find there just aren\'t enough hours in the day to achieve everything I\'d like to. As a result, I don\'t intend to maintain',
+    'a regular blog here as I would hate for it to become stale &amp; stagnant when time constraints prevented me from writing. However, I do love to write and I have plenty to talk',
+    'about, so expect infrequent posts to appear here in something loosely reminiscent of article form.',
+  '</p>',
+  '<div id="photo"><img src="/imgs/dave.jpg"/><span>I look like this</span></div>',  
+  '</div>',
+  '<div id="twitter"><a href="http://twitter.com/shakefon">YOU SHOULD FOLLOW ME ON TWITTER.</a></div>'  
+].join('\n');
+
+this.aea = [
+  '<div id="content">',
+  '<h1>Welcome, fellow <span>An Event Apart</span> attendee.</h1>',
+  '<p class="first hasPhoto">',
+    'I\'m Dave Stevens, a 26 year old web developer who you met at <a href="http://aneventapart.com">An Event Apart</a> in San Diego, CA.  Alternatively, you may have found a business card at the Conference.',
+  '</p>',
+  '<h2>What\'s with the page?</h2>',
+  '<p>',
+    'I often meet folks at these events and have every intention of keeping in touch. However, I\'m not all that great at correctly remembering Twitter handles, email addresses or - and this is the worst part - names.',
+    'So, I figured this time round I would do things right and pass out business cards with a unique URL on them, bringing anyone I interacted with to this page. Thanks for being one of them!',
+  '</p>',
+  '<h2>So who are you again?</h2>',
+  '<p>',
+    'I\'m Dave Stevens. I\'m from a little place called England, but I now call sunny Las Vegas, Nevada my home. By day, I\'m a Front-End Developer at <a href="http://www.zappos.com">Zappos</a>. By night, being utterly devoid of crime-fighting skills, I spend much of my time getting to grips with and thoroughly adoring <a href="http://nodejs.org">node.js</a>.',
+    'The majority of my time is spent working in Javascript, though through the course of a career that has included a strong mobile background, I have made heavy use of HTML, CSS, PHP, a little .NET and JSP in full-time employment for Microsoft, ESPN and on a freelance basis for a multitude of varied customers.', 
+  '</p>',
+  '<h2>And how might we contact you?</h2>',
+  '<p>',
+    'I like receiving <a href="mailto:davestevens84@gmail.com">e-mail [davestevens84@gmail.com]</a>, check <a href="http://twitter.com/shakefon">Twitter [@shakefon]</a> more often than I probably should and can be found on Skype (developerdave).',
+    'If you have another web service you prefer to use, chances are if I\'m on there you\'ll find me under the username "shakefon".',
+  '</p>',
+  '<h2>But why exactly SHOULD we contact you?</h2>',
+  '<p>',
+    'Well, I\'m not one for telling people to do things. I\'d hope that maybe you wanted to continue a discussion we started at AEA, or perhaps you\'ve got an idea you want to get off the ground and maybe I might be someone who could help you with that. Maybe you just love node.js and finding anyone else who does is reason alone. I really don\'t mind why you would choose to contact me, but I welcome your message.',
+  '</p>',
+  '<div id="photo"><img src="/imgs/dave.jpg"/><span>I look like this</span></div>',  
+  '</div>',
+  '<div id="twitter"><a href="http://twitter.com/shakefon">YOU SHOULD FOLLOW ME ON TWITTER.</a></div>'
+].join('');
